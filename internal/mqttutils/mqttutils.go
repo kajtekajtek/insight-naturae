ba021a5f8951b52978c1f0ec85115c5a5b71b028
kajtek <kajtek0123@gmail.com>
2025-01-09 13:20:29 +0100

    refactor: use .env file to load environment variables

--- internal/mqttutils/mqttutils.go
+++ internal/mqttutils/mqttutils.go
@@ -7,12 +7,21 @@ import (
 	"github.com/kajtekajtek/insight-naturae/pkg/utils"
 	"github.com/kajtekajtek/insight-naturae/pkg/models"
 	"strings"
+
+	"github.com/joho/godotenv"
 )
 
 /* loads the MQTT connection options from the environment 
 	variables and returns them as a MqttConn struct */
 func LoadConnOpts() models.MqttConn {
 	var c models.MqttConn
+
+	// load the environment variables from the .env file
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
 	c.Scheme = utils.Getenv("MQTT_SCHEME", "tcp")
 	c.Host = utils.Getenv("MQTT_HOST", "localhost")
 	c.Port = utils.Getenv("MQTT_PORT", "1883")
