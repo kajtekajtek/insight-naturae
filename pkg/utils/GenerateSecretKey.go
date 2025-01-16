6c5ce5c8c8c811fb0772e7c08563492f6e7e66c6
kajtek <kajtek0123@gmail.com>
2025-01-16 17:13:12 +0100

    feat: add GenerateSecretKey function for JWT validation

---
+++ pkg/utils/GenerateSecretKey.go
@@ -0,0 +1,19 @@
+// pkg/utils/GenerateSecretKey.go - encryption secret key generating utility
+package utils
+
+import (
+	"crypto/rand"
+	"fmt"
+)
+
+// generate a length byte long random secret key
+func GenerateSecretKey(length int) ([]byte, error) {
+	key := make([]byte, length)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		return nil, fmt.Errorf("Failed while generating secret key: %v", err)
+	}
+
+	return key, nil
+}
