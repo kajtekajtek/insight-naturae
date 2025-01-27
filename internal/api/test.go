b86f2cdcd5a09c8ce9e0df10259c0b114145d20e
kajtek <kajtek0123@gmail.com>
2025-01-27 19:16:09 +0100

    feat: add SetupUser method to test.go

--- internal/api/test.go
+++ internal/api/test.go
@@ -5,10 +5,17 @@ import (
 	"os"
 	"database/sql"
 	"testing"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"bytes"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"github.com/kajtekajtek/insight-naturae/internal/dbutils"
+	"github.com/kajtekajtek/insight-naturae/internal/middleware"
+	"github.com/kajtekajtek/insight-naturae/pkg/models"
 )
 
 const (
@@ -51,4 +58,38 @@ func SetupRouter(db *sql.DB) *gin.Engine {
 	}
 
 	return r
+}
+
+// register, login and return the token string
+func SetupUser(t *testing.T, db *sql.DB, r *gin.Engine) string {
+	// create the user payload
+	user := models.User{
+		Username: username,
+		Password: password,
+	}	
+	payload, _ := json.Marshal(user)
+
+	// register the user
+	req, _ := http.NewRequest("POST", "/register", bytes.NewBuffer(payload))
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusCreated, w.Code)
+
+	// log in
+	req, _ = http.NewRequest("POST", "/login", bytes.NewBuffer(payload))
+	req.Header.Set("Content-Type", "application/json")
+
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	tokenString := strings.Split(w.Body.String(), "\"")[3]
+	assert.Len(t, tokenString, 135)
+
+	// return the token string
+	return tokenString
 }
