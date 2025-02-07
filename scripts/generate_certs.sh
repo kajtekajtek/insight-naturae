64b4ab74e640a74b2050f29db59e0d5d67a5add5
kajtek <kajtek0123@gmail.com>
2025-02-07 22:25:12 +0100

    refactor: minor change

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -12,8 +12,3 @@ openssl req -x509 -new -nodes -key certs/ca.key -sha256 -days 365 -out certs/ca.
 openssl genrsa -out certs/server.key 2048
 openssl req -new -key certs/server.key -out certs/server.csr -subj "/CN=localhost"
 openssl x509 -req -in certs/server.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/server.crt -days 365 -sha256
-
-# generate mqtt client certificate
-openssl genrsa -out certs/mqtt_client.key 2048
-openssl req -new -key certs/mqtt_client.key -out certs/mqtt_client.csr -subj "/CN=mqtt-client"
-openssl x509 -req -in certs/mqtt_client.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/mqtt_client.crt -days 365 -sha256
